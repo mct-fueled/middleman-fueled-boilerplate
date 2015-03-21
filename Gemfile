4f57f838e6a345ec4e43d2c46e26c36755c515f7
Marc Thomas <hello@mrcthms.com>
2015-03-21 07:07:40 +0000

    Revert

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
-source 'http://rubygems.org'
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
+source 'https://rubygems.org'
 
-gem "middleman", "~> 3.3.2"
+gem "middleman", "~>3.3.10"
 
 # Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
@@ -13,6 +13,6 @@ gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 # Windows does not come with time zone data
 gem "tzinfo-data", platforms: [:mswin, :mingw]
 
-# Willies
 gem "middleman-autoprefixer"
+
 gem "middleman-minify-html"
--- config.rb
+++ config.rb
@@ -24,7 +24,7 @@
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
@@ -63,10 +63,10 @@ set :images_dir, 'assets/images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
